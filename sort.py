b9c182499d0760d19cef11875c7bedb578100aef
DenK0 <denidinamo@gmail.com>
2023-11-17 00:54:18 +0200

    first5

--- sort.py
+++ sort.py
@@ -8,24 +8,23 @@ import sys
 def normalize(name):
     translit_dict = {
         'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g',
-        'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh', 'з': 'z',
+        'д': 'd', 'е': 'e', 'є': 'ye', 'ж': 'zh', 'з': 'z',
         'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k',
         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
         'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f',
         'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
         'ь': '', 'ю': 'iu', 'я': 'ia', 'А': 'A', 'Б': 'B',
         'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E',
-        'Є': 'IE', 'Ж': 'ZH', 'З': 'Z', 'И': 'Y', 'І': 'I',
+        'Є': 'Ye', 'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I',
         'Ї': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М': 'M',
         'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S',
-        'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'KH', 'Ц': 'TS',
-        'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SHCH', 'Ь': '', 'Ю': 'IU',
-        'Я': 'IA'
+        'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts',
+        'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': '', 'Ю': 'Iu',
+        'Я': 'Ya'
     }
 
-    for cyr, lat in translit_dict.items():
-        name = name.replace(cyr, lat)
-
+    translation_table = str.maketrans(translit_dict)
+    name = name.translate(translation_table)
     name = re.sub(r'[^A-Za-z0-9.]+', '_', name)
 
     return name
