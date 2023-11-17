4a8fe7610ec124d0d8097b6bb56d1400d8ecbed0
DenK0 <denidinamo@gmail.com>
2023-11-17 02:17:19 +0200

    first8

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-import os
-import zipfile
-import shutil
+def real_len(string):
+    # Створення списку недозволених символів
+    invalid_chars = ['\n', '\f', '\r', '\t', '\v']
 
-def organize_files(files, destination_path):
-    # Перебираем каждую категорию и список файлов в словаре files
-    for category, file_list in files.items():
-        # Создаем путь категории внутри пути назначения
-        category_path = os.path.join(destination_path, category)
-        # Создаем директорию категории, если она не существует
-        os.makedirs(category_path, exist_ok=True)
+    # Ініціалізація лічильника
+    count = 0
 
-        # Обрабатываем каждый файл в списке файлов
-        for file in file_list:
-            # Создаем полный путь к файлу внутри пути назначения
-            file_path = os.path.join(destination_path, file)
+    # Перебір кожного символу у рядку
+    for char in string:
+        # Перевірка, чи символ не належить до недозволених символів
+        if char not in invalid_chars:
+            # У разі, якщо символ не є недозволеним, збільшуємо лічильник
+            count += 1
 
-            # Если категория - "archives"
-            if category == 'archives':
-                # Извлекаем имя архива без расширения
-                archive_name = os.path.splitext(file)[0]
-                # Создаем путь к архиву внутри категории
-                archive_path = os.path.join(category_path, archive_name)
-                # Создаем директорию архива, если она не существует
-                os.makedirs(archive_path, exist_ok=True)
+    # Повернення кількості символів без недозволених символів
+    return count
 
-                # Если файл является zip-архивом
-                if zipfile.is_zipfile(file_path):
-                    # Распаковываем содержимое архива в путь архива
-                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                        zip_ref.extractall(archive_path)
-                    # Удаляем исходный zip-файл
-                    os.remove(file_path)
-                else:
-                    # Если формат архива не поддерживается, выводим сообщение
-                    print(f"Unsupported archive format: {file}")
 
-            # Если категория не является "archives"
-            else:
-                # Создаем новый путь файла внутри категории
-                new_file_path = os.path.join(category_path, file)
-                # Перемещаем файл в новый путь
-                shutil.move(file_path, new_file_path)
+string1 = 'Alex\nKdfe23\t\f\v.\r'
+string2 = 'Al\nKdfe23\t\v.\r'
+
+print(real_len(string1))
+print(real_len(string2))
